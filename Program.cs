b8e6049c4951caaad8ba1a48d7d54aa528b88472
Ksenia <yoshasoul@gmail.com>
2025-05-03 13:01:23 +0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParallelTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
